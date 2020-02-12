ec8abf76d0f254883cae90cbd0b62409d3ba65b6
bui van huan <chinhlaanh20019718@gmail.com>
2020-02-12 15:56:09 +0700

    create app

---
+++ DemoAspNet/DemoAspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoAspNet.Startup))]
+namespace DemoAspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
